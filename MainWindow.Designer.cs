9d44a96559c42f61559708dde229439a2a1a081e
Boris Arefev <boris.arefev@abbyy.com>
2018-02-20 15:27:29 +0300

    Start

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsLogsViewer
+{
+    partial class WindowsLogsViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WindowsLogsViewer));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.serversList = new System.Windows.Forms.ToolStripButton();
+            this.refreshRate = new System.Windows.Forms.ToolStripComboBox();
+            this.eventLogTable = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLogTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serversList,
+            this.refreshRate});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1021, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // serversList
+            // 
+            this.serversList.Image = global::WindowsLogsViewer.Properties.Resources.connection2;
+            this.serversList.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.serversList.Name = "serversList";
+            this.serversList.Size = new System.Drawing.Size(64, 22);
+            this.serversList.Text = "Servers";
+            this.serversList.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // refreshRate
+            // 
+            this.refreshRate.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.refreshRate.Items.AddRange(new object[] {
+            "Every 30 seconds",
+            "Every 5 minutes",
+            "Every 30 minutes",
+            "Every 2 hours"});
+            this.refreshRate.Name = "refreshRate";
+            this.refreshRate.Size = new System.Drawing.Size(121, 25);
+            this.refreshRate.Click += new System.EventHandler(this.refreshRate_Click);
+            // 
+            // eventLogTable
+            // 
+            this.eventLogTable.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.eventLogTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.eventLogTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.eventLogTable.Location = new System.Drawing.Point(0, 25);
+            this.eventLogTable.Name = "eventLogTable";
+            this.eventLogTable.Size = new System.Drawing.Size(1021, 730);
+            this.eventLogTable.TabIndex = 1;
+            this.eventLogTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // WindowsLogsViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 755);
+            this.Controls.Add(this.eventLogTable);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WindowsLogsViewer";
+            this.Text = "Windows logs viewer";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLogTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton serversList;
+        private System.Windows.Forms.DataGridView eventLogTable;
+        private System.Windows.Forms.ToolStripComboBox refreshRate;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsLogsViewer
+{
+    public partial class WindowsLogsViewer : Form
+    {
+        public WindowsLogsViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            string sourceName = "EventLoggingApp";
+            string machineName = "arefev";
+            String myEventType = null;
+            EventLog myEventLog = new EventLog("Application");
+            myEventLog.Source = sourceName;
+            myEventLog.MachineName = machineName;
+
+            EventLogEntryCollection myLogEntryCollection = myEventLog.Entries;
+            int myCount = myLogEntryCollection.Count;
+            // Iterate through all 'EventLogEntry' instances in 'EventLog'.
+            for (int i = myCount - 1; i > 0; i--)
+            {
+                EventLogEntry myLogEntry = myLogEntryCollection[i];
+                // Select the entry having desired EventType.
+                if (myLogEntry.EntryType.ToString().Equals(myEventType))
+                {
+                    // Display Source of the event.
+                    eventLogTable.Rows.Add(myLogEntry.Message);
+                    return;
+                }
+            }
+
+         }
+
+        private void refreshRate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
